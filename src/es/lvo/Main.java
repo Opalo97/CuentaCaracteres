1689cbd3b8797115ce0f3e953b7aae0bc9286788
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-03-15 12:23:17 +0100

    acabado

--- src/es/lvo/Main.java
+++ src/es/lvo/Main.java
@@ -14,7 +14,15 @@ public class Main {
         char letra = entrada.nextLine().charAt(0);
 
         for (int i = 0; i < texto.length(); i++) {
+            if (letra == texto.charAt(i)) {
+                revision += 1;
+            }
+        }
 
+        if (revision  != 0) {
+            System.out.println("Número de veces que aparece el carácter " + letra + " : " + revision);
+        } else {
+            System.out.println("El caracter " + letra + "no está en el texto");
         }
 
         entrada.close();
